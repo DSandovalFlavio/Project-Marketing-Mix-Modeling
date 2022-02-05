702c9c7244a194936113fa83b558e8b62b4c9d6d
dsandovalflavio <flaviocesarsandoval@gmail.com>
2022-02-05 11:06:50 -0600

    updateAppWeb

--- AppMMM.py
+++ AppMMM.py
@@ -7,6 +7,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import plotly.figure_factory as ff
 
 def cargar_datos():
     """
@@ -15,31 +16,100 @@ def cargar_datos():
     # Cargar datos de la base de datos
     
     path_archivos = "./Data/"
-    InversionMedia = pd.read_csv(path_archivos + "InversionMedia.csv")
-    Sales = pd.read_csv(path_archivos + "Sales.csv")
-    data_CF = pd.merge(InversionMedia, Sales, on="Date", how="left").rename(columns={"Pay TV": "PayTV", "Open TV": "OpenTV"})
+    data_CF = pd.read_csv(path_archivos + "mmm.csv")
     return data_CF
 
 def main():
     # Titulo de la aplicacion
     st.title("Marketing Mix Model (MMM)")
-    st.markdown("""by @dsandovalflavio""")
+    st.markdown("""By [@DSandovalFlavio](https://github.com/DSandovalFlavio) and
+                      [@Chesar832](https://github.com/Chesar832)""")
     # Descripcion de la aplicacion
     st.markdown("""
-                ## Descripcion
-                En este proyecto se realiza paso a paso un marketing mix modeling 
-                que tiene como objetivo obtener las contribuciones de cada uno de los canales 
-                de marketing(Inversion) en las ventas de una empresa, y de esta manera 
-                poder hacer optimizaciones.
+                ## Problema
+                En el Marketing uno de los mayores problemas es la atribución. 
+                Dado que los clientes reciben múltiples impactos online y offline, 
+                no siempre es fácil saber qué canales ofrecen mejores resultados.
+
+                ### *"La mitad del dinero que me gasto en publicidad es un desperdicio: el problema es que no sé qué mitad es"*
+                John Wanamaker
+
+                ## ¿Ques es el Marketing Mix Model (MMM)?
+                El marketing mix modeling es un tipo de modelado que permite conocer 
+                la relación entre cada canal de marketing en los que se invierte dentro 
+                de una empresa y sus respectivos resultados (ventas)
+
+                ### Utilidad del modelo
+                Este modelado es útil porque hace visible los rendimientos con respecto a 
+                inversión y ventas por canal. Además, permite predecir escenarios futuros y 
+                el comportamiento del ROI.
+
+                **Que se obtiene con el modelado?**
+                
+                Se espera obtener curvas de saturación por cada canal disponible en el 
+                conjunto de datos elegido, para conocer qué tipo de patrón de inversión es el más 
+                adecuado para la empresa en cuestión.
+
+                ---
+
+                ## Implementación
+
+                ### Datos
+
                 """)
-    # Mostrar primeras 5 filas de la data en streamlit
+    # Mostrar la data en streamlit
     data = cargar_datos()
-    st.write(data.head())
+    st.write(data)
+    st.markdown("""A continuación se decriben las variables contenidas en el dataset.""")
+    st.markdown("""
+                | **VARIABLE** |                **DESCRIPCIÓN**                      |
+                | :------------|----------------------------------------------------:| 
+                | Date         | Fecha en la que se realizó la inversión             |
+                | OpenTV       | Presupuesto de promoción televisiva abierta.        |
+                | PayTV        | Presupuesto de promoción televisiva de paga.        |
+                | Radio        | Presupuesto de promoción radiofónica                |
+                | Print        | Presupuesto de promoción en medios impresos         |
+                | Facebook     | Presupuesto de promoción en Facebook                |
+                | Google       | Presupuesto de promoción en Google                  |
+                | Email        | Presupuesto de promoción en correos publicitarios   |
+                | Sales        | Ventas obtenidas                                    |
+                """)
+    st.markdown("""
+                ---
+                #### Como a inviertido la marca a lo largo de los años?
+                
+                """)
+    # Data para graficar
+    data_CF = data.copy()
+    data_CF['Date'] = pd.to_datetime(data_CF['Date'])
+    data_CF['year_month'] = data_CF['Date'].dt.strftime('%Y-%m')
+    data_CF['Mes_No'] = data_CF['Date'].dt.month
+    # Mes en texto
+    meses = {1: 'Enero', 2: 'Febrero', 3: 'Marzo', 4: 'Abril', 5: 'Mayo', 6: 'Junio',
+            7: 'Julio', 8: 'Agosto', 9: 'Septiembre', 10: 'Octubre', 11: 'Noviembre', 12: 'Diciembre'}
+    data_CF['Mes'] = data_CF['Mes_No'].map(meses)
+    data_CF['Año'] = data_CF['Date'].dt.year
+    data_CF['Año'] = data_CF['Año'].astype(str)
+    data_CF['Inversion Total'] = data_CF[['Print', 'Email', 'Radio', 'Facebook', 'Google', 'PayTV', 'OpenTV']].sum(axis=1)
+
+    # Comportamiento de la inversion por mes a lo largo de los años
+    com_inv_mes = px.bar(data_CF.groupby(['Año', 'Mes_No', 'Mes'])['Inversion Total'].sum().reset_index(), 
+                        x="Mes", 
+                        y="Inversion Total", 
+                        color="Año",
+                        text="Inversion Total")
+    com_inv_mes.update_traces(texttemplate='%{text:.2s}')
+    st.plotly_chart(com_inv_mes)
+
     # graficar la data en streamlit
-    plot = px.histogram(data, x=['OpenTV','PayTV','Radio'],
-            marginal="box",
-            title="Histograma de " + 'Medios Off')
-    st.plotly_chart(plot)
+    df = data[['Sales','Print', 'Email', 'Radio', 'Facebook', 'Google', 'PayTV', 'OpenTV']].corr().round(2)
+    fig = ff.create_annotated_heatmap( df.values.tolist(),
+                                        x=df.columns.values.tolist(),
+                                        y=df.index.values.tolist(),
+                                        colorscale='Viridis')
+    fig.update_layout(title_text='Correlacion')
+    fig['data'][0]['showscale'] = True
+    st.plotly_chart(fig)
     
 
 if __name__ == '__main__':
